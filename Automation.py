757a85639ff7c48480d7a8e8eb739f257f974612
Janhvi Damkondwar <janhvidamkondwar@gmail.com>
2026-09-15 20:03:32 +0530

    code to move JPG files

--- Automation.py
+++ Automation.py
@@ -1,30 +1,10 @@
 
-# import os
-# import shutil
-
-# source_folder='myfolder'
-# destination_folder='jpg_files'
-
-# #to create new folder
-# os.makedirs(destination_folder,exist_ok=True)
-
-# for files in os.listdir(source_folder):
-#     if files.endswith ('.jpg'):
-#         source_path=os.path.join(source_folder,files)
-#         destination_folder=os.path.join(destination_folder,files)
-#         shutil.move(source_folder,destination_folder)
-# print("All jpg files moved successfully.")
-        
 import os
 import shutil
 
 source_folder = "myfolder"
 destination_folder = "jpg_files"
-
-# Agar myfolder nahi hai to automatically bana dega
 os.makedirs(source_folder, exist_ok=True)
-
-# jpg_files nahi hai to automatically bana dega
 os.makedirs(destination_folder, exist_ok=True)
 
 for file in os.listdir(source_folder):
